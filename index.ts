49c32d9f9c07bfe467e3f5993780def1fba1a87d
Putixx <80105950+Putixx@users.noreply.github.com>
2022-03-09 18:01:15 +0100

    init

---
+++ index.ts
@@ -0,0 +1,23 @@
+const express = require('express')  
+const app = express()  
+app.get('/', function (req, res) {  
+  const num1 = +req.query.num1;
+  const num2 = +req.query.num2;
+  let wynik:number = 0;
+  const operation = req.query.operation;
+
+  if(operation == "dodaj")
+    wynik = num1 + num2;
+  if(operation == "odejmij")
+    wynik = num1 - num2;
+  if(operation == "podziel")
+    wynik = num1 / num2;
+  if(operation == "pomnoz")
+    wynik = num1 * num2;
+
+  res.send(wynik.toString())
+})  
+app.listen(3000)  
+
+
+//res.send('<form method="get"> <label>number1</label><input type="number" name="number1"> <label>number2</label><input type="number" name="number2"> <hr>  <label>operation</label><input type="text" name="operation"> </form>')  
