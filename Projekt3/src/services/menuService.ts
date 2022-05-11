d78267c02860952eb552df285b85467a248d8596
Putixx <80105950+Putixx@users.noreply.github.com>
2022-05-11 21:04:53 +0200

    Meal refactor

---
+++ Projekt3/src/services/menuService.ts
@@ -0,0 +1,111 @@
+/* IMPORT */
+
+import { Meal } from "../models/Meal";
+import { readStorage, updateStorage } from "../services/storageService";
+
+/* FUNCTIONS */
+
+// Create new meal
+export async function createMeal(data: Meal): Promise<number> {
+    const newMeal = new Meal(data);
+    const savedMeals: Meal[] = JSON.parse(await readStorage('./data/menu.json')) ?? [];
+
+    if(savedMeals.find(m => m.name === newMeal.name &&  m.price === newMeal.price &&  m.category === newMeal.category)) {
+        throw new Error("Current meal is already registered!");
+    }
+
+    savedMeals.push(newMeal);
+    await updateStorage('./data/menu.json', JSON.stringify(savedMeals));
+
+    return newMeal.id;
+}
+
+// Read whole menu
+export async function readMenu(): Promise<string> {
+    const savedMeals: Meal[] = JSON.parse(await readStorage('./data/menu.json')) ?? [];
+
+    if(savedMeals.length < 1) {
+      throw new Error("There are no meals in menu!");
+    }
+  
+    let print = "";
+  
+    for(let i = 0; i < savedMeals.length; i++) {
+      print += "ID: " + savedMeals[i].id + " Name: " + savedMeals[i].name + " Price: " + savedMeals[i].price 
+      + " Category: " + savedMeals[i].category + "\n";
+    }
+
+    return print;
+}
+
+// Read existing meal from menu specified by ID
+export async function readMeal(searchID: number): Promise<string> {
+    const savedMeals: Meal[] = JSON.parse(await readStorage('./data/menu.json')) ?? [];
+
+    if(savedMeals.length < 1) {
+      throw new Error("There are no meals in menu!");
+    }
+  
+    const mealIndex = savedMeals.findIndex(m => m.id === searchID)
+  
+    if(mealIndex === -1) {
+      throw new Error("Wrong ID!");
+    }
+  
+    const print = "ID: " + savedMeals[mealIndex].id + " Name: " + savedMeals[mealIndex].name + " Price: " + savedMeals[mealIndex].price 
+    + " Category: " + savedMeals[mealIndex].category + "\n";
+
+    return print;
+}
+
+// Update existing meal from menu specified by ID
+export async function updateMeal(searchID: number, data: Meal): Promise<string> {
+    const savedMeals: Meal[] = JSON.parse(await readStorage('./data/menu.json')) ?? [];
+
+    if(savedMeals.length < 1) {
+         throw new Error("There is no meals in menu!");
+    }
+  
+    const mealIndex = savedMeals.findIndex(r => r.id === searchID)
+  
+    if(mealIndex === -1) {
+        throw new Error("Wrong ID!");
+    }
+  
+    const printOld = "ID: " + savedMeals[mealIndex].id + " Name: " + savedMeals[mealIndex].name + " Price: " + savedMeals[mealIndex].price 
+    + " Category: " + savedMeals[mealIndex].category + "\n";
+  
+    if(data.name) {
+      savedMeals[mealIndex].name = data.name;
+    }
+    if(data.price) {
+      savedMeals[mealIndex].price = data.price;
+    }
+    if(data.category) {
+      savedMeals[mealIndex].category = data.category;
+    }
+    
+    const printNew = "ID: " + savedMeals[mealIndex].id + " Name: " + savedMeals[mealIndex].name + " Price: " + savedMeals[mealIndex].price 
+    + " Category: " + savedMeals[mealIndex].category + "\n";
+  
+    await updateStorage('./data/menu.json', JSON.stringify(savedMeals));
+    return printOld + printNew
+}
+
+// Delete existing meal from menu specified by ID
+export async function deleteMeal(searchID: number): Promise<void> {
+    const savedMeals: Meal[] = JSON.parse(await readStorage('./data/menu.json')) ?? [];
+
+  if(savedMeals.length < 1) {
+    throw new Error("There is no meals in menu!");
+  }
+
+  const mealIndex = savedMeals.findIndex(m => m.id === searchID)
+
+  if(mealIndex === -1) {
+    throw new Error("Wrong ID!");
+  }
+
+  savedMeals.splice(mealIndex, 1);
+  await updateStorage('./data/menu.json', JSON.stringify(savedMeals));
+}
--- Projekt3/src/views/menu.ts
+++ Projekt3/src/views/menu.ts
@@ -2,8 +2,7 @@
 
 import express from "express";
 import { Request, Response } from "express";
-import { Meal } from "../models/Meal";
-import { readStorage, updateStorage } from "../services/storageService";
+import { createMeal, deleteMeal, readMeal, readMenu, updateMeal } from "../services/menuService";
 
 /* SETUP */
 
@@ -26,37 +25,14 @@ menuRouter.post("/register", async function (req: Request, res: Response) {
     return res.status(400).send("Category is missing!");
   }
 
-  const newMeal: Meal = new Meal(JSON.parse(JSON.stringify(req.body)));
-  const savedMeals: Meal[] = JSON.parse(await readStorage('./data/menu.json')) ?? [];
-
-  if(savedMeals.find(m => m.name === newMeal.name &&  m.price === newMeal.price &&  m.category === newMeal.category)) {
-    return res.status(400).send("Current meal is already registered!");
-  }
-
-  savedMeals.push(newMeal);
-  await updateStorage('./data/menu.json', JSON.stringify(savedMeals));
-
-  return res.status(200).send("New meal registration succeded! It's ID: " + newMeal.id);
+  return res.status(201).send("New meal registration succeded! It's ID: " + await createMeal(JSON.parse(JSON.stringify(req.body))));
 });
 
 /* GET */
 
 // GET registered meals
 menuRouter.get("", async function (req: Request, res: Response) {
-  const savedMeals: Meal[] = JSON.parse(await readStorage('./data/menu.json')) ?? [];
-
-  if(savedMeals.length < 1) {
-    return res.status(400).send("There are no meals in menu!");
-  }
-
-  let print = "";
-
-  for(let i = 0; i < savedMeals.length; i++) {
-    print += "ID: " + savedMeals[i].id + " Name: " + savedMeals[i].name + " Price: " + savedMeals[i].price 
-    + " Category: " + savedMeals[i].category + "\n";
-  }
-
-  return res.status(201).send("Menu: \n" + print);
+  return res.status(200).send("Menu: \n" + await readMenu());
 });
 
 // GET registered meal by id
@@ -65,22 +41,7 @@ menuRouter.get("/:id", async function (req: Request, res: Response) {
     return res.status(400).send("You need to send ID!");
   }
   
-  const savedMeals: Meal[] = JSON.parse(await readStorage('./data/menu.json')) ?? [];
-
-  if(savedMeals.length < 1) {
-    return res.status(400).send("There are no meals in menu!");
-  }
-
-  const mealIndex = savedMeals.findIndex(m => m.id === +req.params.id)
-
-  if(mealIndex === -1) {
-    return res.status(400).send("Wrong ID!");
-  }
-
-  const print = "ID: " + savedMeals[mealIndex].id + " Name: " + savedMeals[mealIndex].name + " Price: " + savedMeals[mealIndex].price 
-  + " Category: " + savedMeals[mealIndex].category + "\n";
-
-  return res.status(201).send("Menu: " + print);
+  return res.status(200).send("Menu: " + await readMeal(+req.params.id));
 });
 
 /* PUT */
@@ -94,37 +55,7 @@ menuRouter.put("/:id", async function (req: Request, res: Response) {
     return res.status(400).send("You need to send ID!");
   }
   
-  const savedMeals: Meal[] = JSON.parse(await readStorage('./data/menu.json')) ?? [];
-
-  if(savedMeals.length < 1) {
-    return res.status(400).send("There is no meals in menu!");
-  }
-
-  const mealIndex = savedMeals.findIndex(r => r.id === +req.params.id)
-
-  if(mealIndex === -1) {
-    return res.status(400).send("Wrong ID!");
-  }
-
-  const data = JSON.parse(JSON.stringify(req.body));
-  const printOld = "ID: " + savedMeals[mealIndex].id + " Name: " + savedMeals[mealIndex].name + " Price: " + savedMeals[mealIndex].price 
-  + " Category: " + savedMeals[mealIndex].category + "\n";
-
-  if(data.name) {
-    savedMeals[mealIndex].name = data.name;
-  }
-  if(data.price) {
-    savedMeals[mealIndex].price = data.price;
-  }
-  if(data.category) {
-    savedMeals[mealIndex].category = data.category;
-  }
-  
-  const printNew = "ID: " + savedMeals[mealIndex].id + " Name: " + savedMeals[mealIndex].name + " Price: " + savedMeals[mealIndex].price 
-  + " Category: " + savedMeals[mealIndex].category + "\n";
-
-  await updateStorage('./data/menu.json', JSON.stringify(savedMeals));
-  return res.status(201).send("Meal before edit: " + printOld + " Meal after edit: " + printNew);
+  return res.status(200).send("Meal before and after edit: " + await updateMeal(+req.params.id, JSON.parse(JSON.stringify(req.body))));
 });
 
 /* DELETE */
@@ -135,21 +66,8 @@ menuRouter.delete("/:id", async function (req: Request, res: Response) {
     return res.status(400).send("You need to send ID!");
   }
   
-  const savedMeals: Meal[] = JSON.parse(await readStorage('./data/menu.json')) ?? [];
-
-  if(savedMeals.length < 1) {
-    return res.status(400).send("There is no meals in menu!");
-  }
-
-  const mealIndex = savedMeals.findIndex(m => m.id === +req.params.id)
-
-  if(mealIndex === -1) {
-    return res.status(400).send("Wrong ID!");
-  }
-
-  savedMeals.splice(mealIndex, 1);
-  await updateStorage('./data/menu.json', JSON.stringify(savedMeals));
-  return res.status(201).send("Meal successfuly removed!");
+  await deleteMeal(+req.params.id);
+  return res.status(200).send("Meal successfuly removed!");
 });
 
 export default menuRouter;
